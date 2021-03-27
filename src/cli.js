de02ea613b38d978d76809e7289e4d442e1ecfe0
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2021-03-27 18:54:28 +0430

    initialize cli for wish work

---
+++ src/cli.js
@@ -0,0 +1,81 @@
+import arg from 'arg'
+import inquirer from 'inquirer'
+import {createProject} from "./main";
+
+function parseArgumentIntoOptions(rawArgs) {
+    const args = arg(
+        {
+            '--git': Boolean,
+            '--yes': Boolean,
+            '--install': Boolean,
+            '--sb': Boolean,
+            '-g': '--git',
+            '-y': '--yes',
+            '-i': '--install'
+        },
+        {
+            argv: rawArgs.slice(2)
+        }
+    );
+    return {
+        skipPrompts: args['--yes'] || false,
+        git: args['--git'] || false,
+        template: args._[0],
+        storybook: args['--sb'] || false,
+        runInstall: args['--install'] || false
+    }
+}
+
+async function promptForMissingOptions(options) {
+    const defaultTemplate = "JavaScript";
+    if (options.skipPrompts) {
+        return {
+            ...options,
+            template: options.template || defaultTemplate
+        };
+    }
+
+    let questions = [];
+    if (!options.template) {
+        questions.push({
+            type: 'list',
+            name: 'template',
+            message: 'Please choose which template wish work project to use',
+            choices: ['JavaScript', 'TypeScript'],
+            default: defaultTemplate
+        })
+    }
+
+    if (!options.storybook) {
+        questions.push({
+            type: 'confirm',
+            name: 'storybook',
+            message: 'Initialize Wish Work Design System in your project?',
+            default: false
+        })
+    }
+
+    if (!options.git) {
+        questions.push({
+            type: 'confirm',
+            name: 'git',
+            message: 'Initialize a git repository?',
+            default: false
+        })
+    }
+
+
+    const answer = await inquirer.prompt(questions);
+    return {
+        ...options,
+        template: options.template || answer.template,
+        git: options.git || answer.git,
+        storybook: options.storybook || answer.storybook
+    }
+}
+
+export async function cli(args) {
+    let options = parseArgumentIntoOptions(args);
+    options = await promptForMissingOptions(options);
+    await createProject(options)
+}
---
+++ templates/javascript/README.md
@@ -0,0 +1,44 @@
+# Wish Work React Application
+This app ships with the main React and Storybook configuration files you'll need to get up and running fast. There are standards for front-end development
+that has been initialized by **Wish Work** team for products which is developed by freelancers and developers. In this
+README, we provide useful information so that developers understand the project structure.
+
+## Installation
+First of all open the terminal and write below command:
+```
+mkdir my-wish-app && cd my-wish-app
+```
+and then:
+```npm
+npx @wishwork/create-wish-work-app --install
+```
+After that the project will be initialize. there will be some questions that you should answer.\
+for example which template would you prefer `Javascript` or `Typescript`.
+
+## Structure
+The project structure will be as below: \
+
+    .
+    ├── .storybook             # Storybook configuration
+    ├── src                    # Documentation files (alternatively `doc`)
+    ├── test                   # Automated tests (alternatively `spec` or `tests`)
+    ├── tools                  # Tools and utilities
+    ├── LICENSE
+    └── README.md
+
+Inside a `src` folder would be some other folders as below:
+
+    src
+    ├── common                 # Assets and Mock-API for json-server
+    ├── components             # Reusable components
+    ├── stories                # Stories for getting preview
+    ├── App.js                 # Main renderer
+
+
+## Environment Variables
+
+## Local Development
+
+## Storybook Preview
+
+## Contribute
