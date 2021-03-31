dba4486eb9f6fa1c94e15bef63d407aabfe27739
Amirhosein Shirani <amirhhh.shirani@yahoo.com>
2021-03-31 17:15:39 +0430

    implementing ts + try to add .gitignore

---
+++ templates/typescript/src/components/button/Button.js
@@ -0,0 +1,58 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+import React from 'react';
+import PropTypes from 'prop-types';
+import './button.scss';
+export var Button = function (_a) {
+    var primary = _a.primary, backgroundColor = _a.backgroundColor, size = _a.size, label = _a.label, props = __rest(_a, ["primary", "backgroundColor", "size", "label"]);
+    var mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    return (React.createElement("button", __assign({ type: "button", className: ['storybook-button', "storybook-button--" + size, mode].join(' '), style: { backgroundColor: backgroundColor } }, props), label));
+};
+Button.propTypes = {
+    /**
+     * Is this the principal call to action on the page?
+     */
+    primary: PropTypes.bool,
+    /**
+     * What background color to use
+     */
+    backgroundColor: PropTypes.string,
+    /**
+     * How large should the button be?
+     */
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    /**
+     * Button contents
+     */
+    label: PropTypes.string.isRequired,
+    /**
+     * Optional click handler
+     */
+    onClick: PropTypes.func,
+};
+Button.defaultProps = {
+    backgroundColor: null,
+    primary: false,
+    size: 'medium',
+    onClick: undefined,
+};
